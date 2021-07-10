24de485ca85bf5a4997080c4922e09800654d384
ofer <oferdrr100@g,aol.com>
2021-07-10 07:03:17 -0700

    add login controller

---
+++ LetsEat-OferDrori/Controller/Login/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  LoginViewController.swift
+//  LetsEat-OferDrori
+//
+//  Created by user196234 on 6/29/21.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var errorLbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+	
+        // Do any additional setup after loading the view.
+        setUpElements()
+    }
+    func setUpElements() {
+        errorLbl.alpha=0
+          }
+
+    @IBAction func loginBTNTaped(_ sender: Any) {
+        let email=self.emailTextField.text!
+        let password=self.passwordTextField.text!
+        
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password,completion: {[weak self] result, error in
+            guard let strongSelf = self else{
+                return
+            }
+            guard error == nil else{
+                return
+        }
+        
+            UserData.userEmail=email
+            strongSelf.transitionToMenu()
+
+
+          })
+
+          
+            
+        }
+      
+        
+    
+    func transitionToMenu(){
+        
+        let homeView=storyboard?.instantiateViewController(identifier: "homeBar")
+        view.window?.rootViewController=homeView
+        view.window?.makeKeyAndVisible()
+        
+    }
+}
